4aef87d2ba073925829dd5789763fd839d2dfb53
ctrlouis <lo.bertschy@gmail.com>
2025-11-13 17:48:40 +0100

    Update app title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+    app: {
+        head: {
+            title: 'Time Tracker', // default fallback title
+            htmlAttrs: {
+                lang: 'fr',
+            },
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            ],
+        },
+    },
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+});
